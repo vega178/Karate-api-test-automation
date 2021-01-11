c3d3c7adc25fd9ea11a0b04d60ed838127e54ef0
esteban.vega <2588721a>
2021-01-11 16:31:34 -0500

    fix dev default env

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -2,37 +2,31 @@ function fn() {
   var env = karate.env; // get system property 'karate.env'
   var System = Java.type("java.lang.System");
   var envSystem = System.getProperty("ENVIRONMENT");
-  karate.log('karate.env system property was:', env);
 
-  if (!env) {
+  if (env === null || !env ) {
     env = 'dev';
+  }
+
+  if (env != 'dev') {
+    env = envSystem.toLocaleLowerCase();
   }
 
   var config = {
     baseUrl: 'https://conduit.productionready.io/api'
   }
 
-  //TODO: FALTA RESOLVER EL ISSUE CUANDO NO SE CAMBIA EL ENVIRONMENT DIFERENTE QUE DEV!!
-  if (envSystem.toLocaleLowerCase() != 'dev') {
-    env = envSystem.toLocaleLowerCase();
-  }
-
   switch(env) {
     case 'dev':
       config.userEmail = 'estebanvegapatio@gmail.com';
       config.userPassword = 'Ab1234567!';
+      break;
     case 'qa':
       config.userEmail = 'estebanvegapatio_qa@gmail.com';
       config.userPassword = 'Ab1234567!';
       break;
+    default
+      karate.log('User doesnt exists', env);
   }
-  /*if (env == 'dev') {
-    config.userEmail = 'estebanvegapatio@gmail.com';
-    config.userPassword = 'Ab1234567!';
-  } else if (env == 'qa') {
-    config.userEmail = 'estebanvegapatio_qa@gmail.com';
-    config.userPassword = 'Ab1234567!';
-  }*/
 
   var loginServiceResponse = karate.callSingle('classpath:helpers/login.feature', config).sessionToken;
   karate.configure('headers', {Authorization: 'Token ' + loginServiceResponse});
